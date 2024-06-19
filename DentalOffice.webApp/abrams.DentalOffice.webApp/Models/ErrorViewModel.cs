a40953d8545828ee7ab093a37f02d0267879d09f
tiguila <jtiguila7838@gmail.com>
2024-06-18 23:51:51 -0700

    Upgraded project to MVC arch.

---
+++ DentalOffice.webApp/abrams.DentalOffice.webApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace abrams.DentalOffice.webApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
