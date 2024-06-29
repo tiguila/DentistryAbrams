c55a10d569ed9cc5e6040158bec3be9c4b68abfa
tiguila <jtiguila7838@gmail.com>
2024-06-29 13:31:49 -0700

    Created card model
    This model models a card with different properties (e.g., header, body).

---
+++ DentalOffice.webApp/abrams.DentalOffice.webApp/Models/InformationCardModel.cs
@@ -0,0 +1,14 @@
+﻿namespace abrams.DentalOffice.webApp.Models
+{
+    public class InformationCard
+    {
+        public string Header { get; set; }
+        public List<string> Body { get; set; }
+        public bool IsBullet { get; set; }
+
+        public InformationCard()
+        {
+            Body = [];
+        }
+    }
+}
