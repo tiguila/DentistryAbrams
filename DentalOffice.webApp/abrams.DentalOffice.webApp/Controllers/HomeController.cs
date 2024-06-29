9dea5cfacbdb26aaac142082a05464ea66db6e0e
tiguila <jtiguila7838@gmail.com>
2024-06-29 13:27:34 -0700

    Modified the Home Controller

--- DentalOffice.webApp/abrams.DentalOffice.webApp/Controllers/HomeController.cs
+++ DentalOffice.webApp/abrams.DentalOffice.webApp/Controllers/HomeController.cs
@@ -15,11 +15,14 @@ namespace abrams.DentalOffice.webApp.Controllers
 
         public IActionResult Index()
         {
+            // This just a timestap for the copyright year. Pass a timestap so that the Razor view get the current year to display in the copyright line.
+            ViewData["CurrentTimestamp"] = DateTime.Now;
             return View();
         }
 
         public IActionResult Privacy()
         {
+            ViewData["CurrentTimestamp"] = DateTime.Now;
             return View();
         }
 
@@ -28,5 +31,6 @@ namespace abrams.DentalOffice.webApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
