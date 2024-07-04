f26504b9e7fc496c2c6fd4f95e43a0b08fe1462b
tiguila <jtiguila7838@gmail.com>
2024-07-03 21:07:16 -0700

    Created a Model for pages structure
    Models ease pages content structure(s) throughout this project (Website). They are also quite useful in processing data in the Controllers before passing data to Razor Views.

---
+++ DentalOffice.webApp/abrams.DentalOffice.webApp/Models/InformationCardViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace abrams.DentalOffice.webApp.Models
+{
+    public class InformationCardViewModel
+    {
+        public List<InformationCard> CardOne { get; set; }
+        public List<InformationCard> CardTwo { get; set; }
+        public List<InformationCard> CardThree { get; set; }
+    
+
+        
+
+        public InformationCardViewModel()
+        {
+            CardOne = new List<InformationCard>();
+            CardTwo = new List<InformationCard>();
+            CardThree = new List<InformationCard>();
+        }
+    }
+}
